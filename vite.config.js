c1ceea0436fd3b600d6cd55adc191857f9235487
Muhammad Ahmad <ahmadnaeem6277@gmail.com>
2025-03-27 10:26:27 +0500

    update code

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // Ensures correct routing on Vercel
+  server: {
+    historyApiFallback: true, // Fixes 404 on refresh
+  },
 });
